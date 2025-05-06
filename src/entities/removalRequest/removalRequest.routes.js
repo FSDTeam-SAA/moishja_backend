d9c14bf8ecc9bb0092cb71421db71067a444137c
NavidulHoque <hnavidul@gmail.com>
2025-05-06 09:55:40 +0600

    feat: add user authentication middleware to removal request creation route

--- src/entities/removalRequest/removalRequest.routes.js
+++ src/entities/removalRequest/removalRequest.routes.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { createRemovalRequest } from './removalRequest.controller.js';
+import { userMiddleware, verifyToken } from '../../core/middlewares/authMiddleware.js';
 
 const router = express.Router();
 
-router.post('/create', createRemovalRequest);
+router.post('/create', verifyToken, userMiddleware, createRemovalRequest);
 
 export default router;
